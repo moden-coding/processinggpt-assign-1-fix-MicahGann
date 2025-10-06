05159496f943758590e3a1455f594a93697dcb83
Micah <micahgann3@gmail.com>
2025-10-06 11:30:29 -0400

    done

--- src/App.java
+++ src/App.java
@@ -4,4 +4,24 @@ public class App extends PApplet {
     public static void main(String[] args)  {
         PApplet.main("App");
     }
+    public void settings(){
+        size(400, 400);
+    }
+    public void setup(){
+        background(288, 594);
+        noLoop();
+    }
+    public void draw(){
+        stroke(79, 82, 175);
+        strokeWeight(25);
+        line(300, 300, 200, 200);
+        fill(400, 200, 3);
+        stroke(255);
+        strokeWeight(25);
+        ellipse(200, 200, 50, 50);
+        fill(400, 200, 3);
+        stroke(255);
+        strokeWeight(25);
+        rect(300, 300, 50, 50);
+    }
 }
